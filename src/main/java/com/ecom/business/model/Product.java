384ebc3504fc96f19a168b422c75b3b2dbe48077
suloymandal9 <suloymandal9@gmail.com>
2025-08-14 20:56:23 +0530

    Initial commit

---
+++ src/main/java/com/ecom/business/model/Product.java
@@ -0,0 +1,23 @@
+package com.ecom.business.model;
+
+import jakarta.persistence.*;
+//import jdk.jfr.Category;
+
+import com.ecom.business.model.Category;
+import lombok.Data;
+
+@Entity
+@Data
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+    private String imageUrl;
+    private Double price;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id",nullable =false)
+    private Category category;
+}
